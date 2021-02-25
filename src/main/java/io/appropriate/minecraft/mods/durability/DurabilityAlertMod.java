ffb1cad81ad15bbe40ecefc884af05cc4fd3b0cd
Mike Dillon <mike@appropriate.io>
2021-02-25 01:25:01 -0800

    Initial commit of Durability Alert mod
    Based on fabric-example-mod

---
+++ src/main/java/io/appropriate/minecraft/mods/durability/DurabilityAlertMod.java
@@ -0,0 +1,51 @@
+package io.appropriate.minecraft.mods.durability;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.event.player.AttackBlockCallback;
+
+import net.minecraft.block.BlockState;
+import net.minecraft.item.ItemStack;
+import net.minecraft.text.LiteralText;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Formatting;
+
+public class DurabilityAlertMod implements ClientModInitializer {
+  public void onInitializeClient() {
+    AttackBlockCallback.EVENT.register((player, world, hand, pos, direction) -> {
+      ItemStack stack = player.getMainHandStack();
+      BlockState block = world.getBlockState(pos);
+
+      if (player.isUsingEffectiveTool(block) && stack.isDamaged()) {
+        float breakingSpeed = player.getBlockBreakingSpeed(block);
+        int remainingDamagePercent = calculateRemainingDamagePercent(stack);
+
+        player.sendMessage(
+          new LiteralText(
+            stack.getItem() + " at " + remainingDamagePercent + "% durability, block breaking speed " + breakingSpeed
+          ).formatted(damageColor(remainingDamagePercent)),
+          true
+        );
+      }
+
+      return ActionResult.PASS;
+    });
+  }
+
+  private int calculateRemainingDamagePercent(ItemStack stack) {
+    return (int)Math.round(
+      100 * (stack.getMaxDamage() - stack.getDamage()) / (float)stack.getMaxDamage()
+    );
+  }
+
+  private Formatting damageColor(int remainingDamagePercent) {
+    if (remainingDamagePercent > 75) {
+      return Formatting.GREEN;
+    } else if (remainingDamagePercent > 40) {
+      return Formatting.YELLOW;
+    } else if (remainingDamagePercent > 15) {
+      return Formatting.GOLD;
+    } else {
+      return Formatting.RED;
+    }
+  }
+}
