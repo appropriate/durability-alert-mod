d57b121265cc8e7ed86a548c462e7005d9e104e7
Mike Dillon <mike@appropriate.io>
2021-02-25 20:58:57 -0800

    Move checking logic to DurabilityChecker

--- src/main/java/io/appropriate/minecraft/mods/durability/DurabilityAlertMod.java
+++ src/main/java/io/appropriate/minecraft/mods/durability/DurabilityAlertMod.java
@@ -7,46 +7,32 @@ import net.minecraft.block.BlockState;
 import net.minecraft.item.ItemStack;
 import net.minecraft.text.TranslatableText;
 import net.minecraft.util.ActionResult;
-import net.minecraft.util.Formatting;
 
 public class DurabilityAlertMod implements ClientModInitializer {
   public void onInitializeClient() {
+    final DurabilityChecker checker = new DurabilityChecker();
+
     AttackBlockCallback.EVENT.register((player, world, hand, pos, direction) -> {
       ItemStack stack = player.getMainHandStack();
       BlockState block = world.getBlockState(pos);
 
       if (player.isUsingEffectiveTool(block) && stack.isDamaged()) {
-        float breakingSpeed = player.getBlockBreakingSpeed(block);
-        int remainingDamagePercent = calculateRemainingDamagePercent(stack);
+        DurabilityChecker.Result result = checker.checkItemStack(stack);
 
-        player.sendMessage(
-          new TranslatableText(
-            "durability-alert-mod.messages.alert",
-            stack.getItem().getName(), remainingDamagePercent, breakingSpeed
-          ).formatted(damageColor(remainingDamagePercent)),
-          true
-        );
+        if (result.shouldAlert()) {
+          player.sendMessage(
+            new TranslatableText(
+              "durability-alert-mod.messages.alert",
+              stack.getItem().getName(),
+              result.getRemainingDamagePercent(),
+              player.getBlockBreakingSpeed(block)
+            ).formatted(result.getDamageColor()),
+            true
+          );
+        }
       }
 
       return ActionResult.PASS;
     });
-  }
-
-  private int calculateRemainingDamagePercent(ItemStack stack) {
-    return (int)Math.round(
-      100 * (stack.getMaxDamage() - stack.getDamage()) / (float)stack.getMaxDamage()
-    );
-  }
-
-  private Formatting damageColor(int remainingDamagePercent) {
-    if (remainingDamagePercent > 75) {
-      return Formatting.GREEN;
-    } else if (remainingDamagePercent > 40) {
-      return Formatting.YELLOW;
-    } else if (remainingDamagePercent > 15) {
-      return Formatting.GOLD;
-    } else {
-      return Formatting.RED;
-    }
   }
 }
