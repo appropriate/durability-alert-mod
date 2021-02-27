130155c4cf732dd4b2d2ae5c09764ee4ba60b835
Mike Dillon <mike@appropriate.io>
2021-02-26 23:37:05 -0800

    Switch indentation to Java standard 4 spaces

--- src/main/java/io/appropriate/minecraft/mods/durability/DurabilityAlertMod.java
+++ src/main/java/io/appropriate/minecraft/mods/durability/DurabilityAlertMod.java
@@ -9,30 +9,30 @@ import net.minecraft.text.TranslatableText;
 import net.minecraft.util.ActionResult;
 
 public class DurabilityAlertMod implements ClientModInitializer {
-  public void onInitializeClient() {
-    final DurabilityChecker checker = new DurabilityChecker();
+    public void onInitializeClient() {
+        final DurabilityChecker checker = new DurabilityChecker();
 
-    AttackBlockCallback.EVENT.register((player, world, hand, pos, direction) -> {
-      ItemStack stack = player.getMainHandStack();
-      BlockState block = world.getBlockState(pos);
+        AttackBlockCallback.EVENT.register((player, world, hand, pos, direction) -> {
+            ItemStack stack = player.getMainHandStack();
+            BlockState block = world.getBlockState(pos);
 
-      if (player.isUsingEffectiveTool(block) && stack.isDamaged()) {
-        DurabilityChecker.Result result = checker.checkItemStack(stack);
+            if (player.isUsingEffectiveTool(block) && stack.isDamaged()) {
+                DurabilityChecker.Result result = checker.checkItemStack(stack);
 
-        if (result.shouldAlert()) {
-          player.sendMessage(
-            new TranslatableText(
-              "durability-alert-mod.messages.alert",
-              stack.getItem().getName(),
-              result.getRemainingDamagePercent(),
-              player.getBlockBreakingSpeed(block)
-            ).formatted(result.getDamageColor()),
-            true
-          );
-        }
-      }
+                if (result.shouldAlert()) {
+                    player.sendMessage(
+                        new TranslatableText(
+                            "durability-alert-mod.messages.alert",
+                            stack.getItem().getName(),
+                            result.getRemainingDamagePercent(),
+                            player.getBlockBreakingSpeed(block)
+                        ).formatted(result.getDamageColor()),
+                        true
+                    );
+                }
+            }
 
-      return ActionResult.PASS;
-    });
-  }
+            return ActionResult.PASS;
+        });
+    }
 }
--- src/main/java/io/appropriate/minecraft/mods/durability/DurabilityChecker.java
+++ src/main/java/io/appropriate/minecraft/mods/durability/DurabilityChecker.java
@@ -4,39 +4,39 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.util.Formatting;
 
 public class DurabilityChecker {
-  public Result checkItemStack(ItemStack stack) {
-    Result result = new Result();
-    result.remainingDamagePercent = calculateRemainingDamagePercent(stack);
-    return result;
-  }
-
-  private int calculateRemainingDamagePercent(ItemStack stack) {
-    return (int)Math.round(
-      100 * (stack.getMaxDamage() - stack.getDamage()) / (float)stack.getMaxDamage()
-    );
-  }
-
-  public static class Result {
-    private int remainingDamagePercent;
-
-    public int getRemainingDamagePercent() {
-      return remainingDamagePercent;
+    public Result checkItemStack(ItemStack stack) {
+        Result result = new Result();
+        result.remainingDamagePercent = calculateRemainingDamagePercent(stack);
+        return result;
     }
 
-    public boolean shouldAlert() {
-      return true;
+    private int calculateRemainingDamagePercent(ItemStack stack) {
+        return (int)Math.round(
+            100 * (stack.getMaxDamage() - stack.getDamage()) / (float)stack.getMaxDamage()
+        );
     }
 
-    public Formatting getDamageColor() {
-      if (remainingDamagePercent > 75) {
-        return Formatting.GREEN;
-      } else if (remainingDamagePercent > 40) {
-        return Formatting.YELLOW;
-      } else if (remainingDamagePercent > 15) {
-        return Formatting.GOLD;
-      } else {
-        return Formatting.RED;
-      }
+    public static class Result {
+        private int remainingDamagePercent;
+
+        public int getRemainingDamagePercent() {
+            return remainingDamagePercent;
+        }
+
+        public boolean shouldAlert() {
+            return true;
+        }
+
+        public Formatting getDamageColor() {
+            if (remainingDamagePercent > 75) {
+                return Formatting.GREEN;
+            } else if (remainingDamagePercent > 40) {
+                return Formatting.YELLOW;
+            } else if (remainingDamagePercent > 15) {
+                return Formatting.GOLD;
+            } else {
+                return Formatting.RED;
+            }
+        }
     }
-  }
 }
