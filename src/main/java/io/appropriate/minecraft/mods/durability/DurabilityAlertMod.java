8d9e13769d9d34f3502b66d654038e1e0de43327
Mike Dillon <mike@appropriate.io>
2021-02-28 08:20:04 -0800

    Italicize named items in message

--- src/main/java/io/appropriate/minecraft/mods/durability/DurabilityAlertMod.java
+++ src/main/java/io/appropriate/minecraft/mods/durability/DurabilityAlertMod.java
@@ -5,8 +5,11 @@ import net.fabricmc.fabric.api.event.player.AttackBlockCallback;
 
 import net.minecraft.block.BlockState;
 import net.minecraft.item.ItemStack;
+import net.minecraft.text.MutableText;
+import net.minecraft.text.Text;
 import net.minecraft.text.TranslatableText;
 import net.minecraft.util.ActionResult;
+import net.minecraft.util.Formatting;
 
 public class DurabilityAlertMod implements ClientModInitializer {
     public void onInitializeClient() {
@@ -20,12 +23,18 @@ public class DurabilityAlertMod implements ClientModInitializer {
                 DurabilityChecker.Result result = checker.checkItemStack(stack);
 
                 if (result != null) {
-                    String translationKey = stack.hasCustomName()
-                        ? "messages.durability-alert-mod.alert-named"
-                        : "messages.durability-alert-mod.alert";
+                    String translationKey = "messages.durability-alert-mod.alert";
+                    Text stackName = stack.getName();
+
+                    if (stack.hasCustomName()) {
+                        translationKey = "messages.durability-alert-mod.alert-named";
+                        if (stackName instanceof MutableText) {
+                            stackName = ((MutableText)stackName).formatted(Formatting.ITALIC);
+                        }
+                    }
 
                     TranslatableText message = new TranslatableText(
-                        translationKey, stack.getName(), result.getRemainingDamagePercent());
+                        translationKey, stackName, result.getRemainingDamagePercent());
 
                     player.sendMessage(message.formatted(result.getDamageColor()), true);
                 }
